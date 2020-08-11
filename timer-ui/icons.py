eb98ea655e1c0919a55a6c9edae915ef90626a22
Nick Matantsev <nick.matantsev@gmail.com>
2020-08-11 00:42:15 -0400

    Image loading setup

--- timer-ui/icons.py
+++ timer-ui/icons.py
@@ -1,18 +1,36 @@
+import ctypes
+from sdl2 import *
+from sdl2.sdlimage import *
 import OpenGL.GL as gl
 
 def init_icons():
-    width, height, pixels = (2, 2, bytes([
-        0, 0, 0, 0,
-        255, 255, 255, 255,
-        255, 255, 255, 255,
-        0, 0, 0, 0,
-    ]))
+    rgbaFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA32)
+    rawImage = IMG_Load(b'sample.png')
 
-    icon_texture = gl.glGenTextures(1)
+    try:
+        rgbaImage = SDL_ConvertSurface(rawImage, rgbaFormat, 0)
 
-    gl.glBindTexture(gl.GL_TEXTURE_2D, icon_texture)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, width, height, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, pixels)
+        try:
+            if rgbaImage.contents.format.contents.BytesPerPixel != 4:
+                raise Exception('expecting RGBA')
+
+            surface = rgbaImage.contents
+            pixel_count = surface.w * surface.h * 4
+            pixel_data = ctypes.create_string_buffer(b'', pixel_count)
+            ctypes.memmove(pixel_data, surface.pixels, pixel_count)
+
+            icon_texture = gl.glGenTextures(1)
+
+            gl.glBindTexture(gl.GL_TEXTURE_2D, icon_texture)
+            gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+            gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+            gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, surface.w, surface.h, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, pixel_data.raw)
+
+        finally:
+            SDL_FreeSurface(rgbaImage)
+
+    finally:
+        SDL_FreeSurface(rawImage)
+        SDL_FreeFormat(rgbaFormat)
 
     return icon_texture
