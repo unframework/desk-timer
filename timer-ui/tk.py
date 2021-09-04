af65797d575fc5b10cf81eb9bac99a0667aa13cd
Nick Matantsev <nick.matantsev@gmail.com>
2021-09-04 14:15:11 -0400

    Tweak top-row button sizing

--- timer-ui/tk.py
+++ timer-ui/tk.py
@@ -28,30 +28,39 @@ class Application(tk.Frame):
         self.handIcon = tk.PhotoImage(file='%s/icons/hand.png' %  basePath).zoom(2, 2) # persistent image reference
         self.hourglassIcon = tk.PhotoImage(file='%s/icons/hourglass.png' %  basePath).zoom(2, 2) # persistent image reference
 
-        self.btn1 = tk.Button(self, font=btnFont, compound=tk.LEFT)
+        self.btn1Slot = tk.Frame(self)
+        self.btn1Slot.grid(column=0, row=0, sticky=('N', 'S', 'E', 'W'))
+        self.btn1Slot.pack_propagate(False)
+        self.btn1 = tk.Button(self.btn1Slot, font=btnFont, compound=tk.LEFT)
         self.btn1["text"] = "Menu"
         self.btn1["image"] = self.menuIcon
-        self.btn1.grid(column=0, row=0, sticky=('N', 'S', 'E', 'W'))
+        self.btn1.pack(fill=tk.BOTH, expand=True)
 
         self.btnGap1 = tk.Frame(self)
         self.btnGap1['width'] = TOP_BUTTON_SPACE_W
         self.btnGap1['height'] = TOP_BUTTON_H
         self.btnGap1.grid(column=1, row=0)
 
-        self.btn2 = tk.Button(self, font=btnFont, compound=tk.LEFT)
+        self.btn2Slot = tk.Frame(self)
+        self.btn2Slot.grid(column=2, row=0, sticky=('N', 'S', 'E', 'W'))
+        self.btn2Slot.pack_propagate(False)
+        self.btn2 = tk.Button(self.btn2Slot, font=btnFont, compound=tk.LEFT)
         self.btn2["text"] = "Pause"
         self.btn2["image"] = self.handIcon
-        self.btn2.grid(column=2, row=0, sticky=('N', 'S', 'E', 'W'))
+        self.btn2.pack(fill=tk.BOTH, expand=True)
 
         self.btnGap2 = tk.Frame(self)
         self.btnGap2['width'] = TOP_BUTTON_SPACE_W
         self.btnGap2['height'] = TOP_BUTTON_H
         self.btnGap2.grid(column=3, row=0)
 
-        self.btn3 = tk.Button(self, font=btnFont, compound=tk.LEFT)
+        self.btn3Slot = tk.Frame(self)
+        self.btn3Slot.grid(column=4, row=0, sticky=('N', 'S', 'E', 'W'))
+        self.btn3Slot.pack_propagate(False)
+        self.btn3 = tk.Button(self.btn3Slot, font=btnFont, compound=tk.LEFT)
         self.btn3["text"] = "+1min"
         self.btn3["image"] = self.hourglassIcon
-        self.btn3.grid(column=4, row=0, sticky=('N', 'S', 'E', 'W'))
+        self.btn3.pack(fill=tk.BOTH, expand=True)
 
         self.gap = tk.Frame(self)
         self.gap['height'] = MAIN_GAP_H
